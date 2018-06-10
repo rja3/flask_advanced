31037f270bdcf3b466fb19612a010a3b6506b931
Richard Aplin <aplin@Richards-MBP.lan>
2018-06-10 22:42:49 +0100

    new app

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from booky import create_app, db
 from booky.auth.models import User
 
 
+if __name__ == '__main__':
+    flask_app = create_app('prod')
 
-flask_app = create_app('prod')
+    with flask_app.app_context():
+        db.create_all()
 
-with flask_app.app_context():
-    db.create_all()
     flask_app.run()
 
